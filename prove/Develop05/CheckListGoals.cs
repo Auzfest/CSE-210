6e515ee3c54d0ac1d4695d9babfdd22e659099c8
Auzfest <barnesfamilyaustin@gmail.com>
2023-03-25 08:26:27 -0600

    checklist additon to develop 5

--- prove/Develop05/CheckListGoals.cs
+++ prove/Develop05/CheckListGoals.cs
@@ -13,14 +13,26 @@ public class CheckListGoals : Goal
         string descriptionChoice = Console.ReadLine();
         Console.Write("How many points is it worth each time?: ");
         string pointsChoice = Console.ReadLine();
+        Console.Write("How many times will this be done?: ");
+        string howManyTimesChoice = Console.ReadLine();
 
-        newGoal = string.Format($"CHECKLIST| {goalChoice} | {descriptionChoice}");
+        newGoal = string.Format($"CHECKLIST| {goalChoice} | {descriptionChoice} | 0|{howManyTimesChoice}");
         listOfGoals.Add(newGoal);
         listOfGoals.Add(pointsChoice);
     }
 
     public override string recordEvent(int chosen, List<string> listOfGoals, int level, int points)
     {
+        string[] parts = listOfGoals[chosen].Split("|");
+
+        string timesDone = parts[3];
+        int newDoneTotal = int.Parse(timesDone);
+        string TotalTimes = parts[4];
+        int TotalTimesRequired = int.Parse(TotalTimes);
+        newDoneTotal += 1;
+
+        listOfGoals[chosen] = $"{parts[0]}| {parts[1]} | {parts[2]} | {newDoneTotal}|{parts[4]}";
+
         chosen += 1;
         int newPoints = int.Parse(listOfGoals[chosen]);
         points += newPoints;
@@ -28,6 +40,11 @@ public class CheckListGoals : Goal
         level += dividedPoints;
 
         string newLevel = $"{level}|{points}";
+        if (newDoneTotal == TotalTimesRequired)
+        {
+            listOfGoals[chosen] = "COMPLETE";
+        }
         return newLevel;
+        
     }
 }
